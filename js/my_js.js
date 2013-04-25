143a6ba30de61099270d57add14972e22e85f307
Massih M <massih.z@gmail.com>
2013-04-25 18:59:28 +0200

    Switch from webStorage to IndexedDB

--- js/my_js.js
+++ js/my_js.js
@@ -2,15 +2,138 @@
  * @author massih
  */
 
+$('#dates_page').live('pagebeforecreate', function(event) {
+	// window.indexedDB = window.indexedDB || window.webkitIndexedDB ||
+                   // window.mozIndexedDB;
+// 
+	// if ('webkitIndexedDB' in window) {
+	  // window.IDBTransaction = window.webkitIDBTransaction;
+	  // window.IDBKeyRange = window.webkitIDBKeyRange;
+	// }
+
+	var $dates_set = $("#dates_set");
+	var today = new Date();
+	var startDate = today.getDate();
+	var endDate = getEndDate(today.getMonth(), today.getFullYear());
+	var dayOfWeek = today.getDay();
+	
+	for ( i = startDate; i <= endDate; i++) {
+		var itemId = i + "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
+		var itemTitle = getDayName(dayOfWeek) + "  " + i + "-" + today.getMonthName() + "-" + today.getFullYear();
+		var content = "<div data-role='collapsible' class='my_collapsible_dates' dayOfWeek='"+getDayName(dayOfWeek)+"' id='" + itemId + "'><h3>" + itemTitle + "</h3></div>";
+		$dates_set.append(content);
+		$('#'+itemId).append('<ul data-role="listview" id="'+itemId+'-ul" data-split-icon="minus" data-split-theme="d" data-inset="true" data-divider-theme="d"></ul>');
+		dayOfWeek = (dayOfWeek + 1) % 7;
+	}
+});
+
+$('#dates_page').live('pageinit', function() {
+
+	$('.my-popup-date').hide();
+	
+	if (!window.indexedDB) {
+  		window.alert("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
+  		return false;
+	}
+	
+	var request = window.indexedDB.open("MyTestDatabase");
+	var db;
+	request.onerror = function(event) {
+  		console.log(request.errorCode);
+	};
+	request.onsuccess = function(event) {
+		db = request.result;
+	};
+	
+	
+	$("#taskType").change(function() {
+		if($(this).val() == 'Specific-date'){
+			$('.my-popup-date').hide();
+			$('#specificDate').show();
+		}else if($(this).val() == 'Days-of-week'){
+			$('.my-popup-date').hide();
+			$('#daysOfWeek').show()
+		}
+    	// alert($(this).find("option:selected").text()+' clicked!');
+    	 // alert($(this).val());
+	});
+	
+	$('#clearAll').bind('click',function(){
+		console.log(localStorage.length);
+		localStorage.clear();
+		console.log(localStorage.length);
+	});
+
+	$(".my_collapsible_dates").bind("expand", function(event) {
+		var $element = $('#'+event.target.id+' div');
+		var $dayOfWeek = $('#'+event.target.id).attr('dayOfWeek');
+		var $planned = false;
+		var $ul = $('#'+event.target.id+'-ul');
+
+		if(supports_html5_storage()){
+			if(localStorage[event.target.id] != null){
+				$ul.append('<li><a href="#"> '+localStorage[event.target.id] +'</a><a href="#purchase" data-rel="popup" data-position-to="window" data-transition="pop">Delete item</a> </li>');	
+				$planned = true;
+				// $ul.listview( "refresh" );
+			} 
+			if(localStorage[$dayOfWeek] != null || localStorage["everyday"] != null ){
+				ul.append('<li data-role="list-divider">General Tasks</li>');
+				if(localStorage[$dayOfWeek] != null){
+					$ul.append('<li><a href="#"> '+$dayOfWeek +' job </a><a href="#purchase" data-rel="popup" data-position-to="window" data-transition="pop">Delete item</a></li>');	
+					$planned = true;				
+				}
+				if(localStorage["everyday"] != null){
+					$ul.append('<li><a href="#"> everyday job </a><a href="#purchase" data-rel="popup" data-position-to="window" data-transition="pop">Delete item</a></li>');
+					$planned = true;
+				}
+			}
+			if(!$planned){
+				$ul.append('<li> Do Nothing :)   </li>');
+			}
+			
+		}else{
+			$element.append('<p> HTML5 storage not supported !  </p>');	
+		}
+		localStorage[event.target.id] = "First task !";//TEST ************
+		// console.log(event.target.id);
+		$ul.listview( "refresh" );
+
+	});
+	
+	$(".my_collapsible_dates").bind("collapse", function(event) {
+		$('#'+event.target.id+'-ul').empty();
+	});
+});
+
+
+
+
+
+function save_task(){
+	if (!window.indexedDB) {
+  	  window.alert("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.")
+	}
+	
+}
+
+
+
+
 Date.prototype.getMonthName = function() {
 	var m = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 	return m[this.getMonth()];
 }
-Date.prototype.getDayName = function() {
+function getDayName(day) {
 	var d = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-	return d[this.getDay()];
+	return d[day];
 }
-function get_end_date(month, year) {
+
+function getDayName_short(day) {
+	var d = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+	return d[day];
+}
+
+function getEndDate(month, year) {
 	if (month == 1) {// February only!
 		if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
 			return 29;
@@ -21,19 +144,8 @@ function get_end_date(month, year) {
 }
 
 
-$('#dates_page').live('pagebeforecreate', function(event) {
-	var $dates_set = $("#dates_set");
-	var today = new Date();
-	var start_date = today.getDate();
-	var end_date = get_end_date(today.getMonth(), today.getFullYear());
-	// console.log(today.getDate() + " --- "+ end_date);
-	for ( i = start_date; i <= end_date; i++) {
-		var item_id = i + "-" + (today.getMonth + 1) + "-" + today.getFullYear();
-		var item_title = i + "-" + today.getMonthName() + "-" + today.getFullYear();
-		var content = "<div data-role='collapsible' id='" + item_id + "'><h3>" + item_title + "</h3></div>";
-		// $new_item.append('<h3>' +  + '</h3>');
-		$dates_set.append(content);
-		// $("#dates_set").append($("div "));
-	}
-});
 
+
+function supports_html5_storage() {
+	return ('localStorage' in window) && window['localStorage'] !== null;
+}
